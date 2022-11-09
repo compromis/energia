1daa20acead3ba6ba1f323369f69c6176d9923ab
Guillem <guillem@disedit.com>
2022-11-09 17:29:21 +0100

    Add Hero

--- components/SunnyHero.vue
+++ components/SunnyHero.vue
@@ -1,13 +1,14 @@
 <template>
   <header class="sunny-hero">
-    <div class="night-time" id="night-time" />
-    <GuysSunnyGuy id="sun" class="floaty sunny-guy" />
-    <GuysSolarGuy class="solar-guy" />
-    <GuysSparkle class="floaty sparkle" />
-    <h1 class="campaign-title">
+    <h1 class="campaign-title" data-speed="0.5" data-lag="0.5">
       <span class="campaign-title-tag">Amb l'energia</span>
       <span class="campaign-title-main">Comencem<br>pel sostre</span>
     </h1>
+
+    <div class="night-time" id="night-time" />
+    <GuysSunnyGuy id="sunny-guy" class="floaty sunny-guy" />
+    <GuysPanelGuy id="panel-guy" class="panel-guy" />
+    <GuysSparkle id="sparkle" class="floaty sparkle" />
   </header>
   <div class="campaign-marquee base-level">
     <ClientOnly fallback-tag="span" fallback="Carregant...">
@@ -24,26 +25,73 @@
 <script setup>
 import { gsap } from 'gsap'
 import { CustomEase } from 'gsap/CustomEase'
-gsap.registerPlugin(CustomEase)
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(CustomEase, ScrollTrigger)
 
 onMounted(() => {
-  gsap.from('#sun', {
-    y: '100vh',
-    rotation: 50,
+  gsap.registerPlugin(ScrollSmoother)
+
+  const smoother = ScrollSmoother.create({
+    smooth: 0.5,
+    effects: true
+  })
+
+  gsap.to('#sunny-guy', {
+    top: '10vh',
+    rotation: 0,
+    opacity: 1,
+    y: '',
     duration: 2,
     delay: .5,
     ease: CustomEase.create('custom', 'M0,0 C0,0 0.05,0.228 0.09,0.373 0.12,0.484 0.139,0.547 0.18,0.654 0.211,0.737 0.235,0.785 0.275,0.864 0.291,0.896 0.303,0.915 0.325,0.944 0.344,0.97 0.356,0.989 0.38,1.009 0.413,1.039 0.441,1.058 0.48,1.08 0.496,1.089 0.51,1.091 0.53,1.095 0.552,1.099 0.567,1.101 0.59,1.099 0.623,1.097 0.646,1.094 0.68,1.085 0.768,1.061 0.798,0.998 0.888,0.982 0.964,0.968 1,1 1,1')
-  });
+  })
 
-  gsap.to("#night-time", { opacity: 0, duration: 1, delay: .75, ease: 'none' })
+  gsap.to('#night-time', {
+    opacity: 0,
+    duration: 1,
+    delay: .75,
+    ease: 'none'
+  })
+
+  gsap.to('#panel-guy', {
+    scrollTrigger: {
+      trigger: '.sunny-hero',
+      start: "top top",
+      end: "bottom top",
+      scrub: true
+    },
+    x: '+4vw'
+  })
+
+  gsap.to('#sunny-guy', {
+    scrollTrigger: {
+      trigger: '.sunny-hero',
+      start: 'top top',
+      end: 'bottom top',
+      scrub: true
+    },
+    y: '+40vh'
+  })
+
+  gsap.to('#sparkle', {
+    scrollTrigger: {
+      trigger: '.sunny-hero',
+      start: 'top top',
+      end: 'bottom top',
+      scrub: true
+    },
+    y: '+40vh'
+  })
 })
 </script>
 
 <style lang="scss" scoped>
+/* Sky */
 .sunny-hero {
   position: relative;
   display: grid;
-  background: linear-gradient(to bottom, var(--blue), var(--light-yellow));
+  background-image: var(--daytime-gradient);
   height: calc(100vh - var(--marquee-height));
   place-items: center;
 }
@@ -51,21 +99,12 @@ onMounted(() => {
 .night-time {
   position: absolute;
   inset: 0;
-  background-image: linear-gradient(
-    180deg,
-    hsla(263deg 22% 20%) 0%,
-    hsla(285deg 24% 24% / 97%) 33%,
-    hsla(308deg 28% 27% / 93%) 48%,
-    hsla(323deg 35% 33% / 90%) 58%,
-    hsla(334deg 41% 38% / 85%) 67%,
-    hsla(343deg 44% 43% / 80%) 74%,
-    hsla(352deg 44% 49% / 75%) 81%,
-    hsla(2deg 51% 53% / 75%) 89%,
-    hsla(12deg 62% 54% / 75%) 100%
-  );
+  background-image: var(--nighttime-gradient);
+  pointer-events: none;
   z-index: 2;
 }
 
+/* Hero elements */
 .campaign-title {
   display: flex;
   flex-direction: column;
@@ -107,19 +146,21 @@ onMounted(() => {
   }
 }
 
-.floaty {
-  position: fixed;
-  z-index: 0;
-}
-
+/* Guys */
 .sunny-guy {
-  top: 10vh;
+  position: absolute;
+  top: 100vh;
   right: 10vw;
   width: 15vw;
   min-width: 11rem;
+  opacity: 0;
+
+  svg {
+    width: 100%;
+  }
 }
 
-.solar-guy {
+.panel-guy {
   position: absolute;
   bottom: 0;
   left: 10vw;
@@ -129,6 +170,7 @@ onMounted(() => {
 }
 
 .sparkle {
+  position: absolute;
   top: 20vh;
   left: 15vw;
   width: 4vw;
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
-  <main>
-    <SunnyHero />
-    <div class="base-level">
-      <ChargedIntro />
-    </div>
-  </main>
+  <div id="smooth-wrapper">
+    <main id="smooth-content">
+      <SunnyHero />
+      <div class="base-level">
+        <ChargedIntro />
+      </div>
+    </main>
+  </div>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    { hid: 'gsap', src: 'https://assets.codepen.io/16327/ScrollSmoother.min.js' }
+  ]
+})
+</script>
 
 <style lang="scss" scoped>
 main {
